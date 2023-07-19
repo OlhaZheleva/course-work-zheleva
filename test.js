2e9a8d1b6c1a510850e192765ad5cfcb4c8f8335
OlhaZheleva <dorarashkova@gmail.com>
2023-07-19 14:21:11 +0300

    v4

--- test.js
+++ test.js
@@ -1,43 +1,48 @@
-'use strict';
+"use strict";
 
-// файл чорнетка 
+// файл чорнетка
 
 // Обрати окремі дні
 
+// daysTypeInput.addEventListener("change", numberOfDaysInclusive);
 
+// let start = new Date(startInput.value);
+// let end = new Date(endInput.value);
 
-let startDate = new Date(startInput.value);
-let endDate = new Date(endInput.value);
+// /**
+//  * Загальна фінкція обчислення, враховуючи окремі дні тижня
+//  *
+//  */
 
-function countWeekDaysBetween (startDate, endDate) { 
-    Array
-    .from({ length: (endDate - startDate) / (1000 * 3600 * 24) })
-    .reduce(number => {
-      if (startDate.getDay() % 6 !== 0) number++;
-      startDate = new Date(startDate.setDate(startDate.getDate() + 1));
-      return number;
-    }, 0);
-};
+// function calculateDays(start, end) {
 
-console.log(countWeekDaysBetween(startDate, endDate));
+//     start = new Date(startInput.value);
+//     end = new Date(endInput.value);
+    
+//   const dayType = daysTypeInput.options[daysTypeInput.selectedIndex].value;
 
-/**
- * Обчислення враховуючи тільки будні дні 
- *
- */
+//   switch (dayType) {
 
-function durationWithOnlyWeekday(daysDyWeek) {
+//     case "all":
+//       // проводимо обчислення за звичайною формулою
+//       durationBetweenDates();
+//       break;
+//     case "weekend":
+//     const numberOfDaysInclusive = (start, end) => {
+//         return (
+//           1 + Math.round((end.getTime() - start.getTime()) / (24 * 3600 * 1000))
+//         );
+//       };
+//       const numberOfWeekends = (start, end) => {
+//         const days = numberOfDaysInclusive(start, end); // total number of days
+//         const sundays = Math.floor((days + ((start.getDay() + 6) % 7)) / 7); // number of sundays
+//         return 2 * sundays + (end.getDay() == 6) - (start.getDay() == 0); // multiply sundays by 2 to get both sat and sun, +1 if end is saturday, -1 if start is sunday
+//       };
+//     case "minutes":
+//       const numberOfWeekdays = (start, end) => {
+//         return numberOfDaysInclusive(start, end) - numberOfWeekends(start, end);
+//       };
+//       return numberOfWeekdays; 
+//   }
 
-    daysDyWeek = daysTypeInput.options[daysTypeInput.selectedIndex].value;
-  
-    switch (daysDyWeek) {
-      case "weekdays":
-        countWeekDaysBetween();
-        console.log(countWeekDaysBetween());
-        break;
-      case "weekend":
-        end = new Date(start.setMonth(start.getMonth() + 1));
-        break;
-    }
-  }
-
+// }
